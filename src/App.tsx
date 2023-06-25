f515da21c63a6ec1087c7bd55e22352ab052e467
Maxim Bernatsky <maxsateon@yandex.ru>
2023-06-25 19:06:00 +0300

    create quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,107 @@
 import { useState } from 'react';
 import './index.scss';
 
+interface QuestionsType {
+  title: string;
+  variants: string[];
+  correct: number;
+}
+
+const questions: QuestionsType[] = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: [
+      'приложение',
+      'часть приложения или страницы',
+      'то, что я не знаю что такое',
+    ],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+type ResultProps = {
+  correct: number;
+};
+
+const Result: React.FC<ResultProps> = ({ correct }) => {
+  return (
+    <div className='result'>
+      <img
+        src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png'
+        alt='img'
+      />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href='/'>
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+};
+
+type GameProps = {
+  question: QuestionsType;
+  onClickVariant: (index: number) => void;
+  step: number;
+};
+
+const Game: React.FC<GameProps> = ({ question, onClickVariant, step }) => {
+  const persentage = Math.round((step / questions.length) * 100);
+  return (
+    <>
+      <div className='progress'>
+        <div
+          style={{ width: `${persentage}%` }}
+          className='progress__inner'></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((variant, index) => (
+          <li onClick={() => onClickVariant(index)} key={variant}>
+            {variant}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
 export const App = () => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [step, setStep] = useState<number>(0);
 
-  const openModal = () => {
-    setOpen(true);
+  const [correct, setCorrect] = useState<number>(0);
+
+  const question = questions[step];
+
+  const onClickVariant = (index: number) => {
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
   };
-
-  const closeModal = () => {
-    setOpen(false);
-  };
-
   return (
     <div className='App'>
-      <button onClick={openModal} className='open-modal-btn'>
-        ✨ Открыть окно
-      </button>
-
-      <div className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className='modal'>
-          <svg
-            onClick={closeModal}
-            height='200'
-            viewBox='0 0 200 200'
-            width='200'>
-            <title />
-            <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
-          </svg>
-          <img
-            src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif'
-            alt='gif'
-          />
-        </div>
-      </div>
+      {step !== questions.length ? (
+        <Game question={question} onClickVariant={onClickVariant} step={step} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 };
